c3b9ca8d31edf1f80894a96921e3002faf389d17
Lukasz <luk.perski@gmail.com>
2021-02-13 21:59:34 +0000

    Add equals and hashCode AccountData tests

---
+++ src/test/java/pl/perski/eat/together/model/AccountDataTest.java
@@ -0,0 +1,52 @@
+package pl.perski.eat.together.model;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import pl.perski.eat.together.database.model.AccountData;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class AccountDataTest {
+
+    private static AccountData objectOne;
+    private static AccountData objectTwo;
+    private static AccountData objectThree;
+
+    @BeforeClass
+    public static void setup() {
+        Integer theSameId = 25;
+        Integer differentId = 30;
+
+        objectOne = buildAccountDataWithConstantEmailAndPass(theSameId);
+        objectTwo = buildAccountDataWithConstantEmailAndPass(theSameId);
+        objectThree = buildAccountDataWithConstantEmailAndPass(differentId);
+    }
+
+    private static AccountData buildAccountDataWithConstantEmailAndPass(Integer id) {
+        String email = "test@test.com";
+        String password = "q2D$#$s";
+
+        return AccountData.builder()
+                .id(id)
+                .email(email)
+                .password(password)
+                .build();
+    }
+
+    @Test
+    public void testEquals() {
+        assertEquals(objectOne, objectTwo);
+        assertNotEquals(objectOne, objectThree);
+    }
+
+    @Test
+    public void testHashCode() {
+        int hashCodeOne = objectOne.hashCode();
+        int hashCodeTwo = objectTwo.hashCode();
+        int hashCodeThree = objectThree.hashCode();
+
+        assertEquals(hashCodeOne, hashCodeTwo);
+        assertNotEquals(hashCodeOne, hashCodeThree);
+    }
+}
